14a79fe88b3b1eb7eabb9f04d2152eb7338491e7
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2024-06-29 14:57:53 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () =>{
+    return (
+        <h1 className="text-blue-700 text-4xl">
+            Welcome to React App thats build using Webpack and Babel separately
+        </h1>
+    )
+}
+
+export default App
