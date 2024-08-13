5c11872cb530d7ca01b3afabb488aa23f96d850c
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2024-08-13 11:57:31 -0700

    fixing checkbox styling

--- src/Inputs/Checkbox.js
+++ src/Inputs/Checkbox.js
@@ -1,16 +1,14 @@
-import { useState, useEffect } from "react";
-
 export function Checkbox({ value, text, name, onChange }) {
 	return (
 		<span className="container py-2 mx-auto grid gap-1 grid-cols-8 items-center min-h-6">
-			<label htmlFor={name} className='col-span-5 p-1 text-right italic font-bold'>
+			<label htmlFor={name} className='col-span-6 p-1 text-right italic font-bold'>
 				{text}:
 			</label>
 			<input type='checkbox'
 				name={name}
 				onChange={onChange}
 				defaultChecked={value}
-				className={'text-right w-5 h-5 col-span-3'}
+				className={'text-right w-5 h-5 col-span-2'}
 			/>
 		</span>
 	)
