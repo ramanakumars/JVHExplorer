5da4edeb1bc4cde3ac253c034f2ed487b627f0bf
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2026-06-14 19:59:54 -0700

    adding area and aspect ratio to the plot. Adding a download JSON button

--- src/Explorer/Plotting/PlottableVariables.ts
+++ src/Explorer/Plotting/PlottableVariables.ts
@@ -2,6 +2,8 @@ const plottable_variables: Record<string, { name: string; scale: number }> = {
     angle: { name: "Angle [deg]", scale: 1 },
     physical_width: { name: "Width [km]", scale: 0.001 },
     physical_height: { name: "Height [km]", scale: 0.001 },
+    aspect_ratio: { name: "Aspect Ratio", scale: 1 },
+    physical_area: { name: "Area [km^2]", scale: 1e-6 },
     lon: { name: "Sys III Longitude [deg]", scale: 1 },
     lat: { name: "Planetographic Latitude [deg]", scale: 1 },
     perijove: { name: "Perijove", scale: 1 },
